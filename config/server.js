a4ea8aa8feafded8b31de8f7a186210abe7059a3
pedro-candido <pedroferreirac@hotmail.com>
2021-09-20 15:41:29 -0300

    feat: makes project usable

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6624c5ea05bff60399025844c1fbc1ba'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'aacb3d94-d26e-4aa5-828b-683d9569d794'
+};
